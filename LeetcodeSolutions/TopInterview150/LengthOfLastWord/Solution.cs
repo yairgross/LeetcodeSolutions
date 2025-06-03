614a65488447fd60377617610cbf781328b3439f
yairg <yairgross12@gmail.com>
2025-06-03 22:34:49 +0300

    19. Length Of Last Word

---
+++ LeetcodeSolutions/TopInterview150/LengthOfLastWord/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopInterview150.LengthOfLastWord;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        int len = 0;
+        int i = s.Length - 1;
+        bool foundWord = false;
+
+        while (i >= 0)
+        {
+            if (s[i] == ' ')
+            {
+                if (foundWord)
+                    return len;
+            }
+
+            else
+            {
+                foundWord = true;
+                len++;
+            }
+
+            i--;
+        }
+
+        return len;
+    }
+}
